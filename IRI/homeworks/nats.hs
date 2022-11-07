bfd9d1fb98e157fee6f4783850135c654afc8813
Carlos Pinheiro <pinheirosilvacarlos@gmail.com>
2022-11-07 01:29:22 -0300

    Defining some functions to our nats

--- IRI/homeworks/nats.hs
+++ IRI/homeworks/nats.hs
@@ -1,5 +1,6 @@
 module Nat where
-import Prelude hiding ((+), (*))
+
+import Prelude hiding ((+), (*), (^), (-), pred, min, max)
     
 data Nat = O | S Nat
     deriving (Eq , Show)
@@ -12,7 +13,41 @@ n + S m = S (n + m)
 n * O = O
 n * S m = n + (n * m)
 
+(-) :: Nat -> Nat -> Nat
+m - O = m
+n - (S m) = pred(n - m)
+
+pred :: Nat -> Nat
+pred O = O
+pred (S n) = pred n
+
 fib :: Nat -> Nat
 fib O = O
 fib (S O) = S O
 fib (S (S n)) = fib (S n) + fib n
+
+(^) :: Nat -> Nat -> Nat
+n ^ O = S O
+n ^ S O = n
+n ^ S m = n * (n ^ m)
+
+fact :: Nat -> Nat
+fact O = S O
+fact (S n) = S n * fact n
+
+dist :: Nat -> Nat -> Nat
+dist n O = O
+dist O (S n) = S n
+dist (S n) (S m) = dist n m
+
+min :: Nat -> Nat -> Nat
+min O n = O
+min n O = n
+min (S n) (S m) = S (min (n) (m))
+
+max :: Nat -> Nat -> Nat
+max O n = n
+max n O = n
+max (S n) (S m) = S (max (n) (m))
+
+
